e54f70597c089fa64d2f80174d7954fa80c718b9
mjimmink-coder <mjimmink@gmail.com>
2026-07-18 23:48:07 -0700

    Price Change

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // Live Price IDs, created in the Stripe Dashboard.
 const PRICE_IDS = {
-  pro: 'price_1TsRCEPonndQkh7uS8gCONIo', // Quilt Designer Pro, one-time
+  pro: 'price_1TuoNMPonndQkh7uDL6jNUHT', // Quilt Designer Pro, one-time ($14.99)
   credits: 'price_1TsUdfPonndQkh7uKozmCM5D' // 10 Room Preview Credits, one-time
 };
 
